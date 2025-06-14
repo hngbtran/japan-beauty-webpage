48b56bf9bcc52e61f113c794ef5cc6d8d75d4837
Ho Ngoc Bao Tran <hngbtran@gmail.com>
2025-06-14 19:26:28 +0700

    Make zoom in function works with pressing 'tab'

--- js/zoomImages.js
+++ js/zoomImages.js
@@ -1,20 +1,31 @@
+function loadZoomInFunction() {
+    var images = document.querySelectorAll(".img-hover-container");
+    images.forEach(function(img) {
+        img.tabIndex = 0;
+        img.addEventListener('click', function(event) {
+            zoomIn(this);
+        });
+        img.addEventListener('keydown', function(event) {
+            if (event.key === "Enter" || event.key === " ") {
+                zoomIn(this);
+            }
+        });
+        img.setAttribute('onblur', 'removeZoomIn()')
+    });
+}
+
 function zoomIn(element) {
-    // Check if this element is already zoomed
     const isZoomed = element.classList.contains('zoomed');
-    
-    // Remove zoomed class from all containers
+
     document.querySelectorAll('.img-hover-container.zoomed').forEach(item => {
         item.classList.remove('zoomed');
     });
     
-    // Toggle body class based on whether we're zooming in or out
     if (!isZoomed) {
-        // Add zoomed class to clicked element
         element.classList.add('zoomed');
-        // Add class to body to trigger overlay
         document.body.classList.add('has-zoomed-image');
     } else {
-        // Remove class from body to remove overlay
+        element.classList.remove('zoomed');
         document.body.classList.remove('has-zoomed-image');
     }
 }
